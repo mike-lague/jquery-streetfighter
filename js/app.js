a1e2e5f9bd9b758b25c0599cbd6b7174bb4f21ad
Mike Lague <mike@lague.org>
2015-04-27 12:24:25 -0400

    key down/up behavior, directions (text)

--- js/app.js
+++ js/app.js
@@ -1,25 +1,56 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
-    /* alert('mouse entered .ryu div'); */
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
+    // console.log('mouse entered .ryu div'); 
+    switchImage('.ryu-ready');
   })
   .mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
+    switchImage('.ryu-still');
   })
   .mousedown(function() {
     // console.log('mousedown');
     // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
+    playHadouken();
+    // reveal throwing ryu
+    switchImage('.ryu-throwing');
     // animate haduoken to the right of the screen
+    $('.hadouken').finish().show().animate(
+  		{'left': '1020px'},
+ 		500,
+  		function() {
+    		$(this).hide();
+    		$(this).css('left', '520px');
+  		}
+	);
+   
   })
   .mouseup(function() {
     // console.log('mouseup');
     // ryu goes back to his ready position
-    $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
+    switchImage('.ryu-ready');
+  });
+  $(document).keydown(function(event) {
+  	if (event.which == 88) {
+  		console.log('x is pressed');
+  		switchImage('.ryu-cool');
+  	}
+  })
+  .keyup(function(event) {
+  	if (event.which == 88) {
+  		console.log('x is let go'); 
+  		switchImage('.ryu-still');
+  	}
   });
 });
+ 
+var currimage = '.ryu-still';
+function switchImage (newimage) {
+	$(currimage).hide();
+	$(newimage).show();
+	currimage = newimage;
+}
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
